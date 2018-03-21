0f2cee2d20572d7d570383414cebcb67b42c2662
isberg <alexajak@stud.ntnu.no>
2018-03-21 10:28:05 +0100

    stor fremgang i første powershell script

--- myprocinfo/myprocinfo.ps1
+++ myprocinfo/myprocinfo.ps1
@@ -11,7 +11,7 @@ Write-Host "6 - Hvor mange interrupts fant sted siste sekund?"
 Write-Host "9 - Avslutt dette scriptet"
 }
 
-###########  Main  #########
+###########  Main  ##########
 
 $i = -1 # Sett en startverdi for lup variabel $i
 
@@ -27,14 +27,15 @@ while ( $i -ne 9 )
    {
       1 { Write-Host "Jeg er $($env:USERNAME), dette scriptet heter $($MyInvocation.MyCommand))";  break } #"1 - Hvem er jeg og hva er navnet paa dette scriptet?"
       2 { Write-Host "siste boot time var $((Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime)"}   #"2 - Hvor lenge er det siden siste boot?"
-      3 { "It is three."}
-      4 { "It is four."}
+      3 { Write-Host "Det finnes $( $(((Get-Process).id).length) + $(((Get-Process).Threads.id).length) ) prosesser og tårer"} #"3 - Hvor mange prosesser og traader finnes?"
+      4 { Write-Host "Antall Context Switcher det siste sekundet var: $((Get-CimInstance -ClassName Win32_PerfFormattedData_PerfOS_System).ContextSwitchesPersec)"}
       5 { "It is four."}
-      6 { "It is four."}
+      6 { Write-Host "Antall interrupts siste secund var: $((Get-CimInstance -ClassName Win32_PerfFormattedData_Counters_ProcessorInformation | Where-Object {$_.Name -eq "_Total" }).InterruptsPersec )"}
       9 { $i = 9}
-      default { Write-Host "`n`n------ Feil inputverdi ------"} 
-
+      default { Write-Host "`n`n------ Feil inputtverdi ------"} 
    }
    
 
  }
+
+ exit 0
