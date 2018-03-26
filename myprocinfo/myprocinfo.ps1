2d659916a2be285e38bc87f97fa183c28eba8415
isberg <alexajak@stud.ntnu.no>
2018-03-26 18:11:29 +0200

    fulført kodekvalitetssjekk, endret Write-Hoast til Write-Output, alt ok nå.

--- myprocinfo/myprocinfo.ps1
+++ myprocinfo/myprocinfo.ps1
@@ -14,19 +14,19 @@
  
  # - print meny til consol
 function meny () {
-  Write-Host "`n"  #newline
-  Write-Host "1 - Hvem er jeg og hva er navnet paa dette scriptet?"
-  Write-Host "2 - Hvor lenge er det siden siste boot?"
-  Write-Host "3 - Hvor mange prosesser og traader finnes?"
-  Write-Host "4 - Hvor mange context switch'er fant sted siste sekund?"
-  Write-Host "5 - Hvor stor andel av CPU-tiden ble benyttet i kernelmode og i usermode siste sekund?"
-  Write-Host "6 - Hvor mange interrupts fant sted siste sekund?"
-  Write-Host "9 - Avslutt dette scriptet"
+  Write-Output "`n"  #newline
+  Write-Output "1 - Hvem er jeg og hva er navnet paa dette scriptet?"
+  Write-Output "2 - Hvor lenge er det siden siste boot?"
+  Write-Output "3 - Hvor mange prosesser og traader finnes?"
+  Write-Output "4 - Hvor mange context switch'er fant sted siste sekund?"
+  Write-Output "5 - Hvor stor andel av CPU-tiden ble benyttet i kernelmode og i usermode siste sekund?"
+  Write-Output "6 - Hvor mange interrupts fant sted siste sekund?"
+  Write-Output "9 - Avslutt dette scriptet"
 }
 
 #  - Hvor lenge er det siden siste boot?"
 function oppetid () { 
-  write-host "maskinen har vært oppe i: "
+  Write-Output "maskinen har vært oppe i: "
      # hent verdien "Uptime", formater den og skriv den til consol
   (Get-Uptime).uptime | Format-Table -Property Days, Hours, Minutes, Seconds
 }
@@ -35,19 +35,15 @@ function oppetid () {
 function aktiviteter () {
      # finn basis objekter
   $a = $(Get-Process)   
-
-  Write-Host -noNewline "Det finnes "
      # finn antall av hver type og adder de 2 relevanter tallene fra basis objekt
-  Write-Host -NoNewline " $($(($a).threads.id.length) + $(($a).threads.id.length))" 
-  Write-Host -NoNewline " prosesser og tårer"
-
+  Write-Output "Det finnes  $($(($a).threads.id.length) + $(($a).threads.id.length)) prosesser og tårer" 
 }
 
 #  - Hvor mange context switch'er fant sted siste sekund?
 function context () {
-  Write-Host "Antall Context Switcher det siste sekundet var: "
+  Write-Output "Antall Context Switcher det siste sekundet var: "
     # hent ut og print relevant tall til consol
-  Write-Host -NoNewline "$((Get-CimInstance -ClassName Win32_PerfFormattedData_PerfOS_System).ContextSwitchesPersec)"
+  Write-Output  "$((Get-CimInstance -ClassName Win32_PerfFormattedData_PerfOS_System).ContextSwitchesPersec)"
 }
 
 #  - Hvor mange interrupts fant sted siste sekund?
@@ -57,7 +53,7 @@ function interrupts () {
       # finn relevant objekt og verdi
   $b = $(  ($a | Where-Object {$_.Name -eq "_Total" }).InterruptsPersec )
       # print verdi til consol
-  Write-Host "Antall interrupts siste sekund var: $b"
+  Write-Output "Antall interrupts siste sekund var: $b"
 }
 
 #  - Hvor stor andel av CPU-tiden ble benyttet i Privileged mode og i user mode siste sekund?"
@@ -69,9 +65,9 @@ Function modes () {
       # finn % i user mode + formater med kun 2 desimaltall
   $user = $("{0:F2}" -f ($a | Where-Object {$_.Path -match "user" }).CookedValue )
       # print verdier til consol
-  Write-Host "andel tid i user mode var:  $user %"
-  Write-Host "andel tid i Privileged var:  $Privileged %"
-  Write-Host "resten av tiden var prosessoren i idle mode"
+  Write-Output "andel tid i user mode var:  $user %"
+  Write-Output "andel tid i Privileged var:  $Privileged %"
+  Write-Output "resten av tiden var prosessoren i idle mode"
 }
 
 ##############  Main  ###############
@@ -81,7 +77,7 @@ $i = -1 # Sett en startverdi for lup variabel $i
 while ( $i -ne 9 )
 {
   
-  sleep 2
+  Start-Sleep 2
     # print en meny på consol
   meny  
     # les i brukervalg fra consol
@@ -90,7 +86,7 @@ while ( $i -ne 9 )
   switch ($valg )
    {
       #  - Hvem er jeg og hva er navnet paa dette scriptet?
-      1 { Write-Host "Jeg er $($env:USERNAME), dette scriptet heter $($MyInvocation.MyCommand))";  } 
+      1 { Write-Output "Jeg er $($env:USERNAME), dette scriptet heter $($MyInvocation.MyCommand))";  } 
       #  - Hvor lenge er det siden siste boot?"
       2 { oppetid }  
       #  - Hvor mange prosesser og traader finnes?"
@@ -103,7 +99,7 @@ while ( $i -ne 9 )
       6 { interrupts  }
       #  - Avslutt dette scriptet
       9 { $i = 9}
-      default { Write-Host "`n`n------ Feil inputtverdi ------"} 
+      default { Write-Output "`n`n------ Feil inputtverdi ------"} 
    }   
 
  }
