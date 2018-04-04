ca5f1dd6332f3a37405faceaf0b2e4280b31d183
isberg <alexajak@stud.ntnu.no>
2018-04-04 12:14:54 +0200

    alt utenom README ferdig

--- myprocinfo/myprocinfo.ps1
+++ myprocinfo/myprocinfo.ps1
@@ -6,7 +6,10 @@
   Author: Alexander Jakobsen, 16BITSEC, Studentnr: 473151, e-mail: alexajak@stud.ntnu.no
   Created on March 26, 2018
   Created with Windows powerShell ISE in Windows 10
-
+  
+  Tested in: 
+  PSVersion: 5.1.16299.251
+  PSEdition: Desktop
 #>
 
 
@@ -24,50 +27,64 @@ function meny () {
   Write-Output "9 - Avslutt dette scriptet"
 }
 
-#  - Hvor lenge er det siden siste boot?"
+#  2 - Hvor lenge er det siden siste boot?"
 function oppetid () { 
+
   Write-Output "maskinen har vært oppe i: "
+     
      # hent verdien "Uptime", formater den og skriv den til consol
   (Get-Uptime).uptime | Format-Table -Property Days, Hours, Minutes, Seconds
+
 }
 
-# - Hvor mange prosesser og traader finnes?"
+# 3 - Hvor mange prosesser og traader finnes?"
 function aktiviteter () {
+  
      # finn basis objekter
   $a = $(Get-Process)   
+     
      # finn antall av hver type og adder de 2 relevanter tallene fra basis objekt
-  Write-Output "Det finnes  $($(($a).threads.id.length) + $(($a).threads.id.length)) prosesser og tårer" 
+  Write-Output "Det finnes  $($(($a).length) + $(($a).threads.id.length)) prosesser og tårer" 
+
 }
 
-#  - Hvor mange context switch'er fant sted siste sekund?
+#  4 - Hvor mange context switch'er fant sted siste sekund?
 function context () {
+ 
   Write-Output "Antall Context Switcher det siste sekundet var: "
     # hent ut og print relevant tall til consol
   Write-Output  "$((Get-CimInstance -ClassName Win32_PerfFormattedData_PerfOS_System).ContextSwitchesPersec)"
+
 }
 
-#  - Hvor mange interrupts fant sted siste sekund?
+#  6 - Hvor mange interrupts fant sted siste sekund?
 function interrupts () {
+     
       # fin riktig objekter
   $a = (Get-CimInstance -ClassName Win32_PerfFormattedData_Counters_ProcessorInformation)
       # finn relevant objekt og verdi
-  $b = $(  ($a | Where-Object {$_.Name -eq "_Total" }).InterruptsPersec )
+  $b = $( ($a | Where-Object {$_.Name -eq "_Total" }).InterruptsPersec )
+      
       # print verdi til consol
   Write-Output "Antall interrupts siste sekund var: $b"
+
 }
 
-#  - Hvor stor andel av CPU-tiden ble benyttet i Privileged mode og i user mode siste sekund?"
+#  5 - Hvor stor andel av CPU-tiden ble benyttet i Privileged mode og i user mode siste sekund?"
 Function modes () {
+    
       # ta en måling av bruk
   $a = $(((Get-Counter -counter "\Processor(*)\% Privileged Time", "\Processor(*)\% User Time" ).CounterSamples | Where-Object {$_.InstanceName -match "_total"  }))
       # finn % i Privileged mode + formater med kun 2 desimaltall
   $Privileged = $("{0:F2}" -f ($a | Where-Object {$_.Path -match "privileged" }).CookedValue )
       # finn % i user mode + formater med kun 2 desimaltall
   $user = $("{0:F2}" -f ($a | Where-Object {$_.Path -match "user" }).CookedValue )
+      
       # print verdier til consol
   Write-Output "andel tid i user mode var:  $user %"
   Write-Output "andel tid i Privileged var:  $Privileged %"
   Write-Output "resten av tiden var prosessoren i idle mode"
+
 }
 
 ##############  Main  ###############
--- procmi/procmi.ps1
+++ procmi/procmi.ps1
@@ -4,13 +4,16 @@
   Author: Alexander Jakobsen, 16BITSEC, Studentnr: 473151, e-mail: alexajak@stud.ntnu.no
   Created on March 27, 2018
   Created with Windows powerShell ISE in Windows 10
-
+  
+  Tested in: 
+  PSVersion: 5.1.16299.251
+  PSEdition: Desktop
 #>
 
 ##############  Feilsjekk  ##############
 
-            # Sjekk om det IKKE er medsendt argumenter til scriptet, 
-            # hvis så, print feilmelding og exit fra scipt
+                    # Sjekk om det IKKE er medsendt argumenter til scriptet, 
+                    # hvis så, print feilmelding og exit fra scipt
 if( $args.Length -lt 1 ) {
 
     Write-Output "`nDette scriptet trenger minst et ProsessID argument for å kunne kjøre`n"
@@ -19,40 +22,40 @@ if( $args.Length -lt 1 ) {
 
 ##############  Funksjoner  ##############
 
-            # Får in PID, virituelt minne størelse og WorkingSet størelse som argumenter            
+                    # Får in PID, virituelt minne størelse og WorkingSet størelse som argumenter            
 function tilFil ($prosessID, $vSize, $wSet) {
 
     $dato = $(date -Format yyyyMMdd/Hms)
-            # Lag et filnavn basert på PID og dato
+                    # Lag et filnavn basert på PID og dato
     $navn = "$prosessID--$dato.meminfo"
-            # Lag en fil
+                
+                    # Lag en fil
     Tee-Object $navn
-            # Skriv text til fil
+                    # Skriv text til fil
     Write-Output "******** Minne info om prosess med PID $prosessID ********" >> $navn
     Write-Output "Total bruk av virtuelt minne: $($vSize /1MB) MB" >> $navn
-    Write-Output "Total bruk av virtuelt minne: $wSet KB" >> $navn
+    Write-Output "Total størelse an Working Set i minne: $wSet KB" >> $navn
 
 }
 
 ##############  Main  ##############
 
-            #  Kjør loop 1 gang, pr. medsendte argumenter til scriptet,
+                    #  Kjør loop 1 gang, pr. medsendte argumenter til scriptet,
 foreach ($id in $args) {
-            # Sjekk om PID existerer og send eventuelle feilmeldinger i søpla
-    $test = $(Get-Process -id $id) 2>$null
-            # Hvis PID existerer, hent relevant data og skriv det til fil
-    if($test) {
 
-        $virtualMemory = $((Get-Process -Id $id ).VirtualMemorySize )
-        $workingSet = $((Get-Process -Id $id).WorkingSet)
-                    
-        tilFil $id $virtualMemory $workingSet
+                    # Sjekk om PID existerer og send eventuelle feilmeldinger i søpla
+    $prosessID = $(Get-Process -id $id) 2>$null
+                    # Hvis PID existerer, hent relevant data og skriv det til fil
+    if($prosessID) {
+                                      
+        tilFil $id $(($prosessID ).VirtualMemorySize ) $(($prosessID).WorkingSet)
 
-    }     
-    else {     # Hvis PID ikke exsisterer, print feilmelding til consol
+    }         
+    else {     
+                    # Hvis PID ikke exsisterer, print feilmelding til consol
         Write-Output "finner ingen prosess med ID: $id"  
     }
 
 }
-            # Avslutt script
+                    # Avslutt script
 exit 0
